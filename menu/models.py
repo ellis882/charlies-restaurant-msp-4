210c78764e316cc1986b3af2c893dec8a7e819d7
ellis882 <ellisrachel1304@me.com>
2021-12-02 20:28:16 +0000

    update models from menu app with category

--- menu/models.py
+++ menu/models.py
@@ -3,8 +3,10 @@ from django.utils.text import slugify
 
 
 class Menu(models.Model):
+    objects = models.Manager()
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True)
     price = models.DecimalField(max_digits=5, decimal_places=2)
     slug = models.SlugField(blank=True, null=True)
 
@@ -12,6 +14,22 @@ class Menu(models.Model):
         if not self.slug and self.name:
             self.slug = slugify(self.name)
         super(Menu, self).save(*args, **kwargs)
+
+    class Meta:
+        verbose_name = 'menu'
+        verbose_name_plural = 'menus'
+
+    def __str__(self):
+        return str(self.name)
+
+
+class Category(models.Model):
+    objects = models.Manager()
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
 
     def __str__(self):
         return str(self.name)
--- menu/views.py
+++ menu/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
-from .models import Menu
+from .models import Menu, Category
 
 
 def menu_list(request):
     menu_list = Menu.objects.all()
+    categories = Category.objects.all()
 
-    context = {'menu_list': menu_list, }
+    context = {
+        'menu_list': menu_list,
+        'categories': categories,
+    }
 
     return render(request, 'home/index.html', context)
