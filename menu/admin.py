fd589da1b0ba887791b9996e049dd854d2594ab2
ellis882 <ellisrachel1304@me.com>
2021-12-04 11:06:59 +0000

    customize MenuAdmin

--- menu/admin.py
+++ menu/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
 from .models import Menu, Category
 
-admin.site.register(Menu)
+
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ['name', 'price']
+    search_fields = ['name']
+    list_filter = ['category']
+
+
+admin.site.register(Menu, MenuAdmin)
 admin.site.register(Category)
