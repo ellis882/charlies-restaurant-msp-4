eec9eb10af2ebce19fac3291b5e73f4c8302d6a3
ellis882 <ellisrachel1304@me.com>
2021-12-07 13:27:31 +0000

    changes made at reservation

--- reservation/models.py
+++ reservation/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Reservation(models.Model):
     name = models.CharField(max_length=50)
     email = models.EmailField()
-    phone = models.IntegerField()
+    phone = models.IntegerField(blank=True)
     number_of_persons = models.IntegerField()
     Date = models.DateField()
     time = models.TimeField()
