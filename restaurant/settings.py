4d9f2f9c345255c1fc996fc9c2cd19dbe090e285
ellis882 <ellisrachel1304@me.com>
2021-12-10 22:02:30 +0000

    changed forms for contact and events

--- restaurant/settings.py
+++ restaurant/settings.py
@@ -154,7 +154,7 @@ DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-DEFAULT_FROM_EMAIL = 'testing@example.com'
+DEFAULT_FROM_EMAIL = 'rachelmaiapindus@gmail.com'
 EMAIL_HOST_USER = ''
 EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = False
