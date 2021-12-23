fbae0e2b894ccd3048a0400631284cfd28e0b642
ellis882 <ellisrachel1304@me.com>
2021-12-23 15:11:17 +0000

    made changes in test views

--- contact/test_views.py
+++ contact/test_views.py
@@ -1,7 +1,22 @@
 from django.test import TestCase
+from .views import send_email
+from .forms import ContactForm
 
 
-class TestDjango(TestCase):
+class TestContactForm(TestCase):
 
-    def test_this_thing_works(self):
-        self.assertEqual(1, 1)
+    def test_can_send_message(self):
+        data = {
+            'first_name': 'Jonh',
+            'last_name': 'Doe',
+            'email_address': 'email1@email.com',
+            'message': 'would love to talk about food',
+
+        }
+        response = self.client.post('contact/contact.html', data=data)
+        self.assertTemplateUsed(response, "contact/contact.html")
+        self.assertContains(response, "first_name")
+        self.assertContains(response, "last_name")
+        self.assertContains(response, "email_address")
+        self.assertEqual(ContactForm.objects.count(), 1)
+        self.assertRedirects(response, 'contact:send_success')
