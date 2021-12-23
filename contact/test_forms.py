97908c7da1238c746e53c4d355cd52d5d83b5b6b
ellis882 <ellisrachel1304@me.com>
2021-12-23 12:48:19 +0000

    add test_forms for contactform

--- contact/test_forms.py
+++ contact/test_forms.py
@@ -1,1 +1,21 @@
 from django.test import TestCase
+from contact.forms import ContactForm
+
+
+class TestForms(TestCase):
+
+    def test_contact_form_with_valid_data(self):
+        form = ContactForm(data={
+            'first_name': 'John',
+            'last_name': 'Doe',
+            'email_address': 'email@email.com',
+            'message': 'test1'
+        })
+
+        self.assertTrue(form.is_valid())
+
+    def test_contact_form_with_no_data(self):
+        form = ContactForm(data={})
+
+        self.assertFalse(form.is_valid())
+        self.assertEqual(len(form.errors), 4)
